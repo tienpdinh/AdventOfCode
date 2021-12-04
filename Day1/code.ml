551db27f9d620ae6fd494432b43528177b24c068
Tien Dinh <tiendinhphuc@gmail.com>
2021-12-03 22:46:41 -0600

    finished day 1

--- Day1/code.ml
+++ Day1/code.ml
@@ -1,7 +1,8 @@
 let data = "data.txt"
 
-let get_result init_accum init_prev_val =
-    let f = open_in data in
+let get_result_part_1 init_accum init_prev_val =
+    let f = open_in data
+    in
     let rec build_res accum prev =
         match input_line f with
         | line -> if int_of_string line > prev then build_res (accum + 1) (int_of_string line) else build_res accum (int_of_string line)
@@ -9,4 +10,21 @@ let get_result init_accum init_prev_val =
     in
     build_res init_accum init_prev_val
 
-(* Run this by `get_result 0 max_int` to get the answer *)
+let get_result_part_2 init_accum init_prev_val =
+    let f = open_in data
+    in
+    let rec build_res accum prev count val1 val2 val3 =
+        match input_line f with
+        | line ->
+            if count = 0 then build_res accum prev (count + 1) (int_of_string line) val2 val3
+            else
+            if count = 1 then build_res accum prev (count + 1) val1 (int_of_string line) val3
+            else
+            if count = 2 then build_res accum prev (count + 1) val1 val2 (int_of_string line)
+            else
+                let sum = val1 + val2 + val3
+                in
+                if sum > prev then build_res (accum + 1) sum 3 val2 val3 (int_of_string line) else build_res accum sum 3 val2 val3 (int_of_string line)
+        | exception End_of_file -> close_in f; if (val1 + val2 + val3) > prev then accum + 1 else accum
+    in
+    build_res init_accum init_prev_val 0 0 0 0
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Advent Of Code 2021
+## Day 1
+* Part 1
+```ocaml
+get_result_part_1 0 max_int
+```
+* Part 2
+```ocaml
+get_result_part_2 0 max_int
+```
+## Day 2
